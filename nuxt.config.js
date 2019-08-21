d78defe34deb6aea1f53883107a172ae3d018b3e
shoumen <0showman0@gmail.com>
2019-08-21 14:17:11 +0300

    21.08.19 Fixed google fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Soundblox frontend via nuxt.js' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'}
     ]
   },
   /*
